943a0348490f82ef35214f8a318329de58cf5cc3
haileybaileyo <165137369+haileybaileyo@users.noreply.github.com>
2025-01-06 19:49:05 +0900

    백준2753

--- Main/src/App.java
+++ Main/src/App.java
@@ -1,4 +1,17 @@
-//9498
+// //9498
+// import java.util.Scanner;
+
+// public class App {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         int num = sc.nextInt();
+
+//         System.out.println(num <= 100 && num >= 90  ? "A" : num <= 89 && num >= 80 ? "B" : num <= 79 && num >= 70  ? "C" : num <= 69 && num >= 60  ? "D" : "F");
+//     }
+// } 
+
+//2753
 import java.util.Scanner;
 
 public class App {
@@ -7,6 +20,6 @@ public class App {
 
         int num = sc.nextInt();
 
-        System.out.println(num <= 100 && num >= 90  ? "A" : num <= 89 && num >= 80 ? "B" : num <= 79 && num >= 70  ? "C" : num <= 69 && num >= 60  ? "D" : "F");
+        System.out.println( num % 4 == 0 && num % 100 != 0 || num % 400 == 0 ? "1" : "0");
     }
 } 
