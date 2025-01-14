e4debe1b617cc2fbb6838d3040bef60e402713a5
haileybaileyo <165137369+haileybaileyo@users.noreply.github.com>
2025-01-15 00:32:50 +0900

    백준 11719번

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -25,25 +25,20 @@
 // } 
 
 //프로그래머스_문자열 내 p와 y의 개수
-class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
+// 
 
-       int p = 0;
-       int y = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            int ch = (int)s.charAt(i);
-            if (ch == 80 || ch == 112){
-                p += 1;
-            }
-            else if(ch == 89 || ch == 121){
-                y += 1;
-            }
+//백준 11719번
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
         }
-        answer = p==y? true : false;    
-        
 
-        return answer;
+        scanner.close();
     }
 }
